b1789d43ffefc320c55a408fa460a2e5b68c8c39
Jesse Palmquist <jesse.palmquist@gmail.com>
2016-07-28 20:51:18 -0700

    progress save

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 var port = 3456;
 
-app.use('/static', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/public'));
 
 app.listen(port, function(){ console.log('Server now listening to port ', port); });
